d51a6058b37ac47fdd8c838c93a407ae69fd12fe
alifeee <alifeeerenn@hotmail.com>
2023-07-04 18:46:46 +0100

    add journey.py

---
+++ busbot/journey.py
@@ -0,0 +1,15 @@
+"""Class and methods for journeys
+"""
+
+import datetime
+from dataclasses import dataclass
+from stop import Stop
+
+
+@dataclass
+class Journey:
+    """Bus journey"""
+
+    journey_id: str
+    type: str
+    stops: list[Stop]
