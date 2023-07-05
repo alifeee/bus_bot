c525175513da3a182cb5fc9be0c3ae3269c60010
alifeee <alifeeerenn@hotmail.com>
2023-07-05 17:59:05 +0100

    add journey metadata to stop.py

--- busbot/stop.py
+++ busbot/stop.py
@@ -10,3 +10,8 @@ class Stop:
 
     id: str
     name: str
+
+    # when part of a journey, the stop has a specific journey-unqiue id
+    # and other metadata
+    journey_stop_id: str = None
+    journey_stop_time: datetime.datetime = None
