1005825b1a5072ff47ac20684ca7b92e92c0b956
alifeee <alifeeerenn@hotmail.com>
2023-07-16 23:29:08 +0100

    only poll for journeys including the start/end stop

--- busbot/bothandlers/timetable.py
+++ busbot/bothandlers/timetable.py
@@ -50,11 +50,14 @@ async def _timetable(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await message.edit_text("Start or end stop not set. Use /start.")
         return ConversationHandler.END
 
+    poll_journeys = []
     for journey in all_journeys:
         if start_stop_id not in [s.stop_id for s in journey.stops]:
             continue
         if end_stop_id not in [s.stop_id for s in journey.stops]:
             continue
+
+        poll_journeys.append(journey)
 
         if journey.type == "OUTBOUND":
             start_stop = _get_stop_by_id(journey.stops, start_stop_id)
@@ -66,10 +69,10 @@ async def _timetable(update: Update, context: ContextTypes.DEFAULT_TYPE):
         journey.start_stop = start_stop
         journey.end_stop = end_stop
 
-    capacities = get_journey_capacities(all_journeys)
+    capacities = get_journey_capacities(poll_journeys)
 
     timetable = ""
-    for journey in all_journeys:
+    for journey in poll_journeys:
         start_stop = journey.start_stop
         end_stop = journey.end_stop
 
@@ -84,8 +87,8 @@ async def _timetable(update: Update, context: ContextTypes.DEFAULT_TYPE):
             capacity=capacities[journey.journey_id],
         )
 
-    start_stop_name = all_journeys[0].start_stop.name
-    end_stop_name = all_journeys[0].end_stop.name
+    start_stop_name = poll_journeys[0].start_stop.name
+    end_stop_name = poll_journeys[0].end_stop.name
     await message.edit_text(
         _TIMETABLE_MESSAGE.format(
             start=start_stop_name, end=end_stop_name, timetable=timetable
