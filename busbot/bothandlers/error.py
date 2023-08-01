729e4fe856dedd28d70424a2e4719d0bce7472ab
alifeee <alifeeerenn@hotmail.com>
2023-08-01 18:33:19 +0100

    improve error handling (add traceback)

--- busbot/bothandlers/error.py
+++ busbot/bothandlers/error.py
@@ -1,25 +1,12 @@
 """Error handler: sends "admin" a message with the error and the update that caused it."""
 import os
 import logging
+import traceback
+from typing import Optional
+from telegram import Update
 from telegram.ext import ContextTypes
 
-
-async def error_handler(update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Sends traceback to admin."""
-    bot = context.bot
-    try:
-        admin_id = os.environ["ADMIN_USER_ID"]
-    except KeyError as error:
-        raise ValueError("ADMIN_USER_ID environment variable not set.") from error
-    try:
-        user = update.effective_user
-        chat = update.effective_chat
-    except AttributeError:
-        user = "unknown"
-        chat = "unknown"
-    await bot.send_message(
-        chat_id=admin_id,
-        text=f"""
+_ERROR_MESSAGE = """
 Error!
 
 User: {user}
@@ -28,13 +15,61 @@ Chat: {chat}
 
 Update: {update}
 
-Error: {context.error}
-        """,
-    )
+Job: {job}
+
+Error: {error}
+
+Trace: {trace}
+"""
+
+
+async def error_handler(
+    update: Update | None, context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    """Sends traceback to admin."""
+    bot = context.bot
+    try:
+        admin_id = os.environ["ADMIN_USER_ID"]
+    except KeyError as error:
+        raise ValueError("ADMIN_USER_ID environment variable not set.") from error
+
+    try:
+        tbs = traceback.format_tb(context.error.__traceback__)
+        err_traceback = "".join(tbs)
+    except AttributeError:
+        err_traceback = ""
+
+    if update is not None:
+        await bot.send_message(
+            chat_id=admin_id,
+            text=_ERROR_MESSAGE.format(
+                user=update.effective_user,
+                chat=update.effective_chat,
+                update=update,
+                job="N/A",
+                error=context.error,
+                trace=err_traceback,
+            ),
+        )
+    elif context.job is not None:
+        await bot.send_message(
+            chat_id=admin_id,
+            text=_ERROR_MESSAGE.format(
+                user="N/A",
+                chat="N/A",
+                update="N/A",
+                job=context.job.name,
+                error=context.error,
+                trace=err_traceback,
+            ),
+        )
+
     print(context)
     logger = logging.getLogger(__name__)
     try:
-        logger.error("Update %s caused error %s", update.update_id, context.error)
+        if context.error is not None:
+            logger.error("Update %s caused error %s", update.update_id, context.error)
+            logger.error(context.error)
     except AttributeError:
         logger.error("Update caused error %s", context.error)
     # raise context.error
