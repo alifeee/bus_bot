eac6575c9d22f08d3d60140b1183476f547930a7
alifeee <alifeee.web@outlook.com>
2024-09-26 13:35:56 +0000

    remove_user should also remove chat_data

--- remove_user.py
+++ remove_user.py
@@ -11,13 +11,18 @@ with open("bot_data.pickle", 'rb') as file:
   data = pickle.load(file)
 
 todel = sys.argv[1]
-print(f"Attempting to delete {todel} from bot_data")
 
+print(f"Attempting to delete {todel} from bot_data")
 try:
-  del data["bot_data"][int(todel)]
+  del data["chat_data"][int(todel)]
 except:
-  print("could not delete")
-  sys.exit(1)
+  print("could not delete from bot_data")
+
+print(f"Attempting to delete {todel} from chat_data")
+try:
+  del data["chat_data"][int(todel)]
+except:
+  print("could not delete from chat_data")
 
 print("re-saving pickle file")
 with open("bot_data.pickle", "wb") as file:
