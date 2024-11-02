68a7eefa73e3b9ab14a5b833dbfbaa21ed1f8bc0
alifeee <alifeee.web@outlook.com>
2024-11-02 14:08:45 +0000

    BUG: remove_user did not remove from bot_data - fixed

--- remove_user.py
+++ remove_user.py
@@ -1,4 +1,5 @@
 import pickle
+import traceback
 import json
 import sys
 from pprint import pprint
@@ -14,15 +15,25 @@ todel = sys.argv[1]
 
 print(f"Attempting to delete {todel} from bot_data")
 try:
-  del data["chat_data"][int(todel)]
-except:
+  del data["bot_data"][int(todel)]
+  print("  deleted")
+except KeyError:
+  print("  key error")
+except Exception as e:
   print("could not delete from bot_data")
+  print("exception: ", e)
+  print(traceback.format_exc(e))
 
 print(f"Attempting to delete {todel} from chat_data")
 try:
   del data["chat_data"][int(todel)]
-except:
-  print("could not delete from chat_data")
+  print("  deleted")
+except KeyError:
+  print("  key error")
+except Exception as e:
+  print("  could not delete from chat_data")
+  print("  exception: ", e)
+  print(traceback.format_exc(e))
 
 print("re-saving pickle file")
 with open("bot_data.pickle", "wb") as file:
