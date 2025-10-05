f7c4358a54cd798f43260bcb585a96f190f8e7bd
alifeee <alifeee@alifeee.net>
2025-10-05 17:36:23 +0100

    add paused reply :]

--- bot.py
+++ bot.py
@@ -9,13 +9,18 @@ from dotenv import load_dotenv
 from telegram import *
 from telegram.ext import *
 
+DO_PAUSED_REPLY = True
+
 from busbot.busapi import Credentials
-from busbot.bothandlers.start import start_handler
-from busbot.bothandlers.timetable import timetable_handler
-from busbot.bothandlers.track import track_handler
-from busbot.bothandlers.whois import whois_handler
+
 from busbot.bothandlers.error import error_handler
-from busbot.reminderer import initialise_job
+
+if not DO_PAUSED_REPLY:
+    from busbot.bothandlers.start import start_handler
+    from busbot.bothandlers.timetable import timetable_handler
+    from busbot.bothandlers.track import track_handler
+    from busbot.bothandlers.whois import whois_handler
+    from busbot.reminderer import initialise_job
 
 # I don't use this but you can use it to set commands with @botfather with /setcommands
 _ = """
@@ -43,6 +48,22 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+# YWxpZmVlZQ==
+async def _reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        """Hi!
+    
+Thanks for using bus bot over the years :]
+
+Bus bot has stopped working properly as the bus provider has updated their online API.
+
+You are welcome to fix bus bot. If you'd like more information, please get in contact.
+
+Yours truly,
+YWxpZmVlZQ=="""
+    )
+
+
 def main():
     """Start the bot!"""
     persistent_data = PicklePersistence(
@@ -54,14 +75,17 @@ def main():
         Application.builder().token(API_KEY).persistence(persistent_data).build()
     )
 
-    application.add_handler(start_handler)
-    application.add_handler(timetable_handler)
-    application.add_handler(track_handler)
-    application.add_handler(whois_handler)
+    if DO_PAUSED_REPLY:
+        application.add_handler(MessageHandler(None, _reply))
+
+    else:
+        application.add_handler(start_handler)
+        application.add_handler(timetable_handler)
+        application.add_handler(track_handler)
+        application.add_handler(whois_handler)
+        initialise_job(application.job_queue)
 
     application.add_error_handler(error_handler)
-
-    initialise_job(application.job_queue)
 
     application.run_polling()
 
